7f9b58c7d5df51c4fecfe73b03ccc785637e3da4
Mike Upchurch <mupchrch@users.noreply.github.com>
2018-08-12 19:07:16 -0400

    updates log in link href to pass callback param

--- public/script/component/Home.js
+++ public/script/component/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class Home extends React.Component {
   render() {
+    const logInUrl = '/api/login?callback=' + encodeURIComponent(document.location.origin + '/api/callback');
+
     return (
       <div>
         <h2>welcome to dndtextui</h2>
         <br />
-        <a href="/api/login">log in</a>
+        <a href={logInUrl}>log in</a>
       </div>
     );
   }
